dfa96c58c52aac84fb9fa9c9aaf932cc2e65fe13
hacker586 <sagarbank74@gmail.com>
2024-11-04 17:53:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'temp';
+  constructor(){}
+  task = new FormControl('');  
+  taskList : any =[];
+  completedTask : any =[];
+  alertMessage : string = ""; 
+  getTask(){
+    if(this.taskList?.includes(this.task?.value)){
+      this.alertMessage = "'"+this.task?.value+ "' Task already exist in the Remaining task List";  
+      // alert("'"+this.task?.value+ "' Task already exist in the Remaining task List");
+    }else if(this.completedTask?.includes(this.task?.value)){
+      this.alertMessage = "'"+this.task?.value+ "' Task already exist in the Completed task List";  
+      // alert("'"+this.task?.value+ "' Task already exist in the Completed task List");
+    }else{
+      this.taskList.push(this.task?.value);
+      this.task?.setValue('');
+      this.task?.reset();
+    }
+  }
+  done(doneTask: any){
+    this.completedTask.push(doneTask);
+    this.taskList = this.taskList.filter((task: any) => task !== doneTask);
+  }
+  backToTaskList(data: any){
+    this.taskList.push(data);
+    this.completedTask = this.completedTask.filter((task: any) => task !== data);
+  }
+  closeAlert(){
+    this.alertMessage ="";
+  }
 }
